1c75c2523ba06f1660a5b691906494d596d9b5b4
pinguxx28 <riskumax@gmail.com>
2023-04-18 17:37:31 +0300

    add menu files

---
+++ src/menu.c
@@ -0,0 +1,12 @@
+#include "menu.h"
+
+#include <stdio.h>
+
+void display_menu(void)
+{
+	printf("1. Fetch\n");
+	printf("2. Update\n");
+	printf("3. Display\n");
+	printf("4. Generate\n");
+	printf("5. Erase\n");
+}
---
+++ src/menu.h
@@ -0,0 +1,15 @@
+/*
+ * menu.h - contains functions for displaying and handling the base program
+ *
+ * Copyright (c) 2023 pinguxx28
+ *
+ * This program is released under the GNU General Public License, version 3.0.
+ * See the LICENSE file for more details.
+ */
+
+#ifndef MENU_H
+#define MENU_H
+
+void display_menu(void);
+
+#endif /* MENU_H */
