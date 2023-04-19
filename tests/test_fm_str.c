8c254b74d0d4bdd880c284981086b88bfebb5e84
pinguxx28 <riskumax@gmail.com>
2023-04-19 19:18:30 +0300

    add test case for fm_streq

--- tests/test_fm_str.c
+++ tests/test_fm_str.c
@@ -13,5 +13,11 @@ void test_fm_str(void)
 	TEST(fm_strlen(a, 2) == 2, TEST_FM_STR);
 
 
-
+	/* fm_streq */
+	TEST( fm_streq("hello", "hello", 10), TEST_FM_STR);
+	TEST(!fm_streq("hello", "world", 10), TEST_FM_STR);
+	TEST(!fm_streq("hel"  , "helta", 10), TEST_FM_STR);
+	TEST( fm_streq("hella", "hello",  3), TEST_FM_STR);
+	TEST( fm_streq("hello", "hello",  0), TEST_FM_STR);
+	TEST( fm_streq("abdis", "abbeb",  0), TEST_FM_STR);
 }
